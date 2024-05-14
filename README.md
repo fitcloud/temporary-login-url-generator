bab72e795b9c730b8beeebd3ea2334f201a9ecce
soohyuneeee <soohyuneeee12@gmail.com>
2024-05-14 15:54:17 +0900

    임시 로그인 URL return application

---
+++ README.md
@@ -0,0 +1,32 @@
+# 임시 로그인 URL을 반환하는 애플리케이션 입니다.
+일시적으로 계정에 접근하여 로그아웃 또는 세션 만료 시 접근 불가합니다.
+
+
+# 해당 애플리케이션 사용 방법
+
+``` bash
+git clone git@github.com:fitcloud/temporary-login-url-generator.git & cd emporary-login-url-generator
+```
+해당 명령줄을 `cloudshell`에서 실행합니다.
+<br><br>
+```bash
+aws configure
+```
+해당 명령줄을 실행한 후 미리 발급받은 `accessKey`와 `secretKey`를 입력합니다.<br>
+`accessKey`와 `secretKey` 발급 방법은 가이드를 참고해주세요.
+<br><br>
+```bash
+python main.py
+```
+해당 명령줄을 실행 후 파라미터 값에 응답합니다.
+<br><br>
+```bash
+세션 만료 시간을 입력해주세요. 기본값은 6시간 입니다. : 
+```
+세션이 유효할 원하는 시간을 입력합니다. 입력하지 않으면 기본인 6시간으로 설정됩니다.
+<br><br>
+```bash
+다음 URL을 복사해 주세요: {URL값}
+```
+URL값을 복사해서 해당 담당자에게 전송해주세요.<br>
+
--- main.py
+++ main.py
@@ -5,63 +5,76 @@ from urllib.parse import urlencode
 
 
 def get_account_url():
-    credentials = get_role_sts()
-    print(credentials)
-    return get_switch_url(credentials) if credentials else None
+    # 사용자로부터 세션 만료 시간을 입력 받습니다. 기본값은 6시간입니다.
+    duration_hours = int(input('세션 만료 시간을 입력해주세요. 기본값은 6시간 입니다. : ').strip() or 6)
+    duration_seconds = duration_hours * 3600
+
+    # AWS STS(Security Token Service)를 통해 임시 자격 증명을 가져옵니다.
+    credentials = get_federation_credentials(duration_seconds)
+
+    # 가져온 자격 증명을 기반으로 로그인 URL을 생성합니다.
+    return get_login_url(credentials, duration_seconds) if credentials else None
 
 
-def get_role_sts():
+def get_federation_credentials(duration_seconds):
+    # AWS STS 클라이언트를 생성합니다.
     sts_client = boto3.client('sts')
-    res = sts_client.get_caller_identity()
-    print(res)
     try:
+        # 연합 토큰을 요청하여 임시 자격 증명을 가져옵니다.
         response = sts_client.get_federation_token(
-            Name='Saltware_federation_user',
-            DurationSeconds=10000,
+            Name='Saltware_federation_user',  # 연합 토큰을 요청하는 사용자의 이름
+            DurationSeconds=duration_seconds,  # 토큰의 유효 기간(초)
             PolicyArns=[
-                {
-                    'arn': 'arn:aws:iam::aws:policy/AdministratorAccess'
-                }
+                {'arn': 'arn:aws:iam::aws:policy/AdministratorAccess'}  # 사용자에게 적용할 IAM 정책의 ARN 목록
             ]
         )
         return response['Credentials']
     except Exception as e:
-        print(f"Error assuming role: {e}")
+        print(f"역할을 가져오는 동안 오류가 발생했습니다: {e}")
         return None
 
 
-def get_switch_url(sts):
+def get_login_url(credentials, duration_seconds):
+    # AWS 콘솔 및 로그인 URL
     console_url = "https://console.aws.amazon.com/console/home"
     sign_in_url = "https://signin.aws.amazon.com/federation"
 
+    # 세션 매개변수 설정
     session_parameters = {
-        "sessionId": sts['AccessKeyId'],
-        "sessionKey": sts['SecretAccessKey'],
-        "sessionToken": sts['SessionToken']
+        "sessionId": credentials['AccessKeyId'],  # 액세스 키 ID
+        "sessionKey": credentials['SecretAccessKey'],  # 비밀 액세스 키
+        "sessionToken": credentials['SessionToken']  # 세션 토큰
     }
-    encoded_session = urlencode({"Session": json.dumps(session_parameters)})
-    session_duration = str(6 * 60 * 60)
+    encoded_session = urlencode({"Session": json.dumps(session_parameters)})  # 세션 매개변수를 JSON 형식으로 인코딩합니다.
+    session_duration = str(duration_seconds)  # 세션의 유지 기간을 초 단위로 지정합니다.
 
+    # 로그인 토큰을 가져오는 URL 설정
     get_signin_token_url = f"{sign_in_url}?Action=getSigninToken&SessionDuration={session_duration}&SessionType=json&{encoded_session}"
 
     try:
+        # 로그인 토큰을 요청하여 가져옵니다.
         response = requests.get(get_signin_token_url)
-        # Raises an HTTPError if the HTTP request returned an unsuccessful status code
         response.raise_for_status()
-        signin_token = response.json().get('SigninToken')
-        if not signin_token:
-            print("SigninToken not found in response")
-            return None
-        signin_token_parameter = urlencode({"SigninToken": signin_token})
-        destination_parameter = urlencode({"Destination": console_url})
-        return f"{sign_in_url}?Action=login&{signin_token_parameter}&{destination_parameter}"
+        signin_token = response.json().get('SigninToken')  # 로그인 토큰을 가져옵니다.
+        if signin_token:
+            signin_token_parameter = urlencode({"SigninToken": signin_token})  # 로그인 토큰을 매개변수로 인코딩합니다.
+            destination_parameter = urlencode({"Destination": console_url})  # 대상을 매개변수로 인코딩합니다.
+            # 로그인 URL을 반환합니다.
+            return f"{sign_in_url}?Action=login&{signin_token_parameter}&{destination_parameter}"
+        else:
+            print("로그인 토큰을 가져오는 데 실패했습니다.")
     except requests.RequestException as e:
-        print(f"HTTP Request failed: {e}")
+        print(f"HTTP 요청이 실패했습니다: {e}")
     except json.JSONDecodeError:
-        print("Failed to decode JSON from response")
+        print("응답에서 JSON을 디코딩하는 데 실패했습니다.")
     return None
 
 
+# AWS 계정 URL을 가져옵니다.
 url = get_account_url()
 
-print(url)
+# 가져온 URL을 출력합니다.
+if url:
+    print("다음 URL을 복사해 주세요:", url)
+else:
+    print("AWS 계정 URL을 가져오는 동안 오류가 발생했습니다.")
